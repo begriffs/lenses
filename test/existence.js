b68d3fc5770dcc38f328620267205d3b0aca2752
Joe Nelson <cred+github@begriffs.com>
2014-03-31 19:24:01 -0700

    New module structure

---
+++ test/existence.js
@@ -0,0 +1,10 @@
+var L    = require('../lenses'),
+  chai   = require('chai'),
+  expect = chai.expect;
+
+describe('Lenses', function() {
+  'use strict';
+  it('exists', function() {
+    expect(L).to.be.an('object');
+  });
+});
